d847af15a9115125014126a63ac97636607aa215
Tetraclor <kdvikt@gmail.com>
2021-06-16 17:16:35 +0300

    init

---
+++ WebHelloApp/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHelloApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.MapWhen(
+                context => context.Request.Query.ContainsKey("name"),
+                app => app.Run(SayHello)
+                );
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+
+        private async Task SayHello(HttpContext context)
+        {
+            var name = context.Request.Query["name"];
+            await context.Response.WriteAsync($"Hello {name}");
+        }
+    }
+}
